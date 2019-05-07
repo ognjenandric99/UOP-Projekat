ab271709416784bae748e295d63646c7f444d9fb
BUDI <BUDI@DESKTOP-9L9PE4U>
2019-05-07 20:53:02 +0200

    Odradjen login do pola

--- DomZdravlja/src/startPackage/Main.java
+++ DomZdravlja/src/startPackage/Main.java
@@ -3,6 +3,7 @@ package startPackage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Scanner;
 
 import accounts.Doktor;
 import accounts.Korisnik;
@@ -13,14 +14,45 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("LOGIN FUNKCIJA22222");
-		System.out.println("LOGIN FUNKCIJA22222");
+			login();
 			String accountovi = getText("src/accounts/accounts.txt");
-			System.out.println(accountovi);
 	}
 	public static Boolean login() {
-		System.out.println("Da");
-		return true;
+		System.out.println("Unesite Vase podatke da bi ste se prijavili na sistem.");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Username : ");
+		String loginusername = scanner.nextLine();
+		
+		System.out.println("Password : ");
+		String loginpassword = scanner.nextLine();
+		
+		scanner.close();
+		
+		String accountovi = getText("src/accounts/accounts.txt");
+		String[] accountovi1=accountovi.split(";");
+		
+		for(int i=0;i<accountovi1.length;i++) {
+			String trenutnavrednost = accountovi1[i];
+			String[] accountovi2 = trenutnavrednost.split("\\|");
+			if(loginusername.equalsIgnoreCase(accountovi2[0])&& loginpassword.equalsIgnoreCase(accountovi2[1])) {
+				if(accountovi2[3].equals("Pacijent")) {
+					System.out.println("Pacijent");
+				}
+				else if (accountovi2[3].equals("MedicinskaSestra")) {
+					System.out.println("Medicinska Sestra");
+				}
+				else if (accountovi2[3].equals("Doktor")) {
+					System.out.println("Doktor");
+				}
+				else {
+					System.out.println("Doslo je do greske, pokusajte ponovo.");
+					login();
+				}
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	public static String getText(String path) {
 		String getTextText = "";
