f9166b2af79f552f5e8dda4138d7a5f6074cf614
ognjenandric99 <48236995+ognjenandric99@users.noreply.github.com>
2019-05-09 09:24:29 +0200

    Update MedicinskaSestra.java

--- DomZdravlja/src/accounts/MedicinskaSestra.java
+++ DomZdravlja/src/accounts/MedicinskaSestra.java
@@ -1,6 +1,9 @@
 package accounts;
 
 import startPackage.SluzbaSestre;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MedicinskaSestra extends Zaposleni {
 	private SluzbaSestre Sluzba;
@@ -14,18 +17,35 @@ public class MedicinskaSestra extends Zaposleni {
 	}
 
 
-	public void dodajKorisnika() {
+	public void dodajKorisnika(String username,String password, String jmbg, String tip, String ime, String prezime, String adresa, String brtelefona,String pol) {
 		try {
 			File file = new File("src/accounts/accounts.txt");
 			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-			writer.write("BUDI|BUDI123|123456789123|Pacijent|Ognjen|Andric|Malina 13, Sremska Kamenica|0615561450|true");
+			writer.write(username+"|"+password+"|"+jmbg+"|"+tip+"|"+ime+"|"+prezime+"|"+adresa+"|"+brtelefona+"|"+pol);
 			writer.close();
 			} catch (IOException e) {
 			e.printStackTrace();
 			}
 	}
-	public void zakaziPregled() {
-		System.out.println("Zakazat pregled.");
+	public void zakaziPregled(String idpregleda,String pacijent,String doktor, String opis, String soba, String termin, String status) {
+		try {
+			File file = new File("src/ostalo/pregledi.txt");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			writer.write(idpregleda+"|"+pacijent+"|"+doktor+"|"+opis+"|"+soba+"|"+termin+"|"+status);
+			writer.close();
+			} catch (IOException e) {
+			e.printStackTrace();
+			}
+	}
+	public void dodajknjizicu(String idknjizice, String datumisteka, String kategorija) {
+		try {
+			File file = new File("src/ostalo/knjizice.txt");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			writer.write(idknjizice+"|"+kategorija+"|"+datumisteka);
+			writer.close();
+			} catch (IOException e) {
+			e.printStackTrace();
+			}
 	}
 	public void izdajRacun() {
 		System.out.println("Racun izdat");
