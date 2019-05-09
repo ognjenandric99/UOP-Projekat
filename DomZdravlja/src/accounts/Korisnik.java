32f24c3ee1589f770ea2abcf7287424e634ea950
sf53-2018 <sf53-2018@307-18.mshome.net>
2019-05-09 08:20:49 +0200

    Korisnik promenjen u abstract

--- DomZdravlja/src/accounts/Korisnik.java
+++ DomZdravlja/src/accounts/Korisnik.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
-public class Korisnik {
+public abstract class Korisnik {
 	private String Ime,Prezime,Jmbg,Adresa,BrojTelefona,Username,Password,Uloga;
 	private Boolean Pol;
 	public Korisnik(String ime, String prezime, String jmbg, String adresa, String brojTelefona, String username,
